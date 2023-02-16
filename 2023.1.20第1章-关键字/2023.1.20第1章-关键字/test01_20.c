f32294563f2902f0061e3ebee00e53f53fbd187b
xianyuxiedaima <2502138417@qq.com>
2023-02-16 22:01:29 +0800

    2023.2.16

---
+++ 2023.1.20第1章-关键字/2023.1.20第1章-关键字/test01_20.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<Windows.h>
+
+//int main()
+//{
+//	printf("hello world!\n");
+//	system("pause");
+//	return 0;
+//}
+
+int g_val = 100;
+int main()
+{
+	int a = 10;
+	printf("%d\n",g_val);
+	system("pause");
+	return 0;
+}
